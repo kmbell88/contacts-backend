084c0b3c17596a16752353c31e6117f2f2c07f96
kmbell88 <kmbell88@gmail.com>
2021-03-17 19:14:12 -0700

    Updated ts config

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import contactsV1Routes from '../api/v1/routes/contacts.routes';
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT : String | number = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
