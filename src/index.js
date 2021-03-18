51bca43de1935f2afc4542259be57aaccc7e34b7
kmbell88 <kmbell88@gmail.com>
2021-03-17 20:58:03 -0700

    updated index.js and package.json

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const contacts_routes_1 = __importDefault(require("../api/v1/routes/contacts.routes"));
 const app = express_1.default();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(morgan_1.default('dev'));
@@ -15,7 +15,7 @@ app.use('/api/v1/contacts', contacts_routes_1.default);
 app.get('/', (req, res) => {
     res.json({ message: "Hello World!" });
 });
-app.listen(PORT, () => {
-    console.log(`Listening on Port: ${PORT}`);
+app.listen(port, () => {
+    console.log(`Listening on Port: ${port}`);
 });
 exports.default = app;
